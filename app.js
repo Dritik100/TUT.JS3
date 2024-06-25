621175cdef671e2718bd538da1440a617d75c35f
Dritik100 <ritikdubey2664@gmail.com>
2024-06-25 23:41:07 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,91 @@
+// const student={
+//     name: "Ritik",
+//     age:23,
+//     english: 98,
+//     physics:89,
+//     Maths:78,
+//     getAvg(){
+//         let avg=(this.english+ this.physics+ this.Maths)/3;
+//         // console.log(avg);
+//         console.log(`${this.name} got avg marks= ${avg}`);
+//     }
+// }
+
+// try & catch statment;
+
+// console.log("hello");
+// console.log("hello");
+// console.log("hello");
+
+// console.log(a);
+// console.log("hello2");
+// console.log("hello2");
+// console.log("hello2");
+
+// console.log("hello");
+// console.log("hello");
+// console.log("hello");
+// try{
+//     console.log(a);
+// } catch{
+//     console.log("caught an error.... a is not define");
+// }
+
+// console.log("hello2");
+// console.log("hello2");
+// console.log("hello2");
+
+// Arrow function example
+
+// const sum=(a,b) =>{
+//     console.log(a+b);
+// };
+
+// const cube=(n) => {
+//     return n*n*n;
+// }
+
+// const power=(a,b) => {
+//     return a**b;
+// }
+
+
+// implicit return 
+
+// const mul=(a,b) => (
+//     a*b
+// );
+
+// const cube=(n) => (
+//     n*n*n
+// );
+
+// settime function
+
+// console.log("HI there!");
+
+// setTimeout( () => {
+//     console.log("Apna college");
+// },6000)
+
+// console.log("welcome to");
+
+
+// setinterval time;;
+
+// console.log("HI there!");
+
+// setInterval( () => {
+//     console.log("Apna college");
+// },2000)
+console.log("HI there!");
+let id1=setInterval( () => {
+    console.log("Apna college");
+},2000);
+console.log(id1);
+
+ let id2=setInterval( () => {
+    console.log("Apna vicharwar");
+},3000);
+console.log(id2);
+
